1149ee881494598f33ff7163473746661abb5966
sohayelmahmud <sohayelmahmud612@gmail.com>
2025-11-27 20:26:03 +0600

    init

--- Python/05_Math/07_TriangleQuest.py
+++ Python/05_Math/07_TriangleQuest.py
@@ -5,3 +5,10 @@ domain    : Python
 author    : Sohayel Mahmud
 created   : 26 Nov, 2025
 '''
+
+
+
+
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i - 1)//9)**2)
